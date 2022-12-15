9b3a4ede85d6216d4fb840075226c8d5eae4f7f5
hsg32 <hsg32@vt.edu>
2022-12-15 14:21:14 -0800

    Finished day 15 part 2

--- day15/day15-pt2.py
+++ day15/day15-pt2.py
@@ -7,7 +7,7 @@ import time
 
 # Globals
 # 4_000_000
-XY_MAX = 20
+XY_MAX = 4_000_000
 XY_MIN = 0
 
 class Sensor:
@@ -26,6 +26,18 @@ def taxiDistance(p1, p2):
     x_diff = abs(p1[0] - p2[0])
     y_diff = abs(p1[1] - p2[1])
     return x_diff + y_diff
+
+
+def find_gap_with_size(ranges, gap):
+    s = sorted(ranges, key=lambda x: x[0])
+    max_x = s[0][1]
+    for i in range(1, len(s) - 1):
+        if s[i][0] == max_x + gap + 1:
+            return (True, max_x + 1)
+        if s[i][1] < max_x:
+            continue
+        max_x = s[i][1]
+    return (False, -1)
 
 def main(input_file_name):
     input_file = open(input_file_name)
@@ -47,9 +59,10 @@ def main(input_file_name):
 
     # Next, loop over every row
     for y in range(0, XY_MAX + 1):
-        print(f"Checking y={y}")
-        curr_range = range(XY_MIN, XY_MAX + 1)
-        # curr_range = filterfalse(lambda x: False, curr_range)
+        if y % 100_000 == 0:
+            print(f"Checking y={y}...")
+        # Grab the list of ranges for the covered cells by each sensor on the current row
+        curr_ranges = []
         for sensor in sensors:
             dist_2_y_row = abs(sensor.pos[1] - y)
             # Ignore sensors that are too far to care about current row
@@ -57,25 +70,16 @@ def main(input_file_name):
                 continue
             x_delta = sensor.taxi_radius - dist_2_y_row
             x_min, x_max = (sensor.pos[0] - x_delta, sensor.pos[0] + x_delta)
-
-            before = time.time()
-            new_range = filterfalse(lambda x: x_min <= x <= x_max, curr_range)
-            # _, curr_range = tee(temp)
-            after = time.time()
-            # print(f"Took {after - before} seconds")
-            # curr_range = new_range
-            _new_range, curr_range = tee(new_range)
-            # before = time.time()
-            list(_new_range)
-            # after = time.time()
-            # print(f"Took {after - before} seconds")
-            # curr_range = new_range
+            curr_ranges.append((x_min, x_max))
         
-        found_x = list(curr_range)
-        if len(found_x) > 0:
-            distress_beacon = (found_x[0], y)
+        # Check if there is a gap of size 1 within the covered cells
+        # If so, that must be our distress beacon
+        found_gap, gap = find_gap_with_size(curr_ranges, 1)
+        if found_gap:
+            distress_beacon = (gap, y)
             break
 
+    print(f"Distress beacon is at {distress_beacon}")
     tuning_frequency = distress_beacon[0] * XY_MAX + distress_beacon[1]
     print("The tuning frequency is {}.".format(tuning_frequency))
 
